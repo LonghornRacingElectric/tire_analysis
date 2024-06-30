85a8ba5467da17e0c2595ab2d66a3334f463212f
Robert Horvath <roberthorvath5@gmail.com>
2024-06-30 02:49:05 -0500

    TANYL-1: Added load falloff plots

--- workspace.py
+++ workspace.py
@@ -1,4 +1,7 @@
+import numpy as np
+import matplotlib.pyplot as plt
 from tire_toolkit import workflows
+from tire_toolkit.assets.tire_model.MF52 import MF52
 
 tires = workflows.import_tires(
     tire_names = 
@@ -27,3 +30,26 @@ workflows.plot_tires(
     FZ_min = 100, 
     FZ_max = 1091, 
     file_name = "First_Principles_Metrics.pdf")
+
+# new_tire = MF52("Hoosier_16_7.5x7_R25B_12psi", "./tire_toolkit/assets/tir_files/Round_8_Hoosier_R25B_16x7p5_10_on_7in_12psi_PAC02_UM2.tir")
+
+# SA_sweep = np.linspace(-20 * np.pi / 180, 20 * np.pi / 180, 100)
+# FZ_sweep = np.linspace(100, 4000, 100)
+
+# SA_vals = []
+
+# print(new_tire.tire_eval(FZ = 1091, alpha = 15 * np.pi / 180, kappa = 0, gamma = 0)[1])
+
+# for load in FZ_sweep:
+#     SA_vals.append(max(new_tire.tire_eval(FZ = load, alpha = SA_sweep, kappa = 0, gamma = 0)[1]))
+
+# plt.figure()
+
+# plt.plot(FZ_sweep, SA_vals)
+# plt.title("Peak FY vs FZ")
+# plt.xlabel("Normal Load (N)")
+# plt.ylabel("Peak Lateral Force (N)")
+
+# plt.savefig("Peak_FY_vs_FZ.png")
+
+# plt.show()
