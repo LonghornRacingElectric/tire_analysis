8f1cfb95e8e11cd3bc46c704dc39a5448da1f330
robert <roberthorvath5@gmail.com>
2024-08-18 00:57:12 -0500

    Updated package to only include MF52

--- setup.py
+++ setup.py
@@ -9,6 +9,6 @@ setup(name='LHR_tire_toolkit',
       author_email='rhorvath@utexas.edu',
       url='https://github.com/LonghornRacingElectric/tire_analysis',
       packages=['tire_model', 'tire_model/file_processing', 'tire_model/MF52_calculations'],
-      py_modules=['LHR_tire_toolkit/process_tire', 'LHR_tire_toolkit/workflows']
+      py_modules=['tire_model/MF52']
       # packages=find_packages(exclude=("assets", "__pycache__",)),
      )
